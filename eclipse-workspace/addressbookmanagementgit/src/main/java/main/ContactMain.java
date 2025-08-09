593497bd646ad0faf06f9ec1f4844e7675726578
231fa04331 <charanpadarthi1430@gmail.com>
2025-08-09 19:26:53 +0530

    code added in main

--- eclipse-workspace/addressbookmanagementgit/src/main/java/main/ContactMain.java
+++ eclipse-workspace/addressbookmanagementgit/src/main/java/main/ContactMain.java
@@ -1,5 +1,14 @@
 package main;
 
-public class ContactMain {
+import controller.ContactController;
+import model.ContactDatabase;
+import view.ContactView;
 
+public class ContactMain {
+    public static void main(String[] args) {
+        ContactDatabase db = new ContactDatabase();
+        ContactView view = new ContactView();
+        ContactController controller = new ContactController(db, view);
+        controller.start();
+    }
 }
